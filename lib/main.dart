3611ea1baa671b7acc86540c6567e265adc422ee
alishaawal1 <aleeshaawall455@gmail.com>
2023-12-06 10:02:34 +0545

    Calculator App

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:calculator/view/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const CalculatorView(),
+        
+      },
+    ),
+  );
+}
